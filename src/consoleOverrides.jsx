e45236e7e12b22e62318e262fa72e267d4edc2cf
hp <riteshgharti121232@gmail.com>
2024-10-08 09:41:37 +0530

    checking carousel

--- src/consoleOverrides.jsx
+++ src/consoleOverrides.jsx
@@ -1,11 +1,11 @@
-const originalConsoleError = console.error;
+// const originalConsoleError = console.error;
 
-// Override console.error
-console.error = (...args) => {
-  // Suppress specific error messages
-  if (/defaultProps/.test(args[0])) {
-    return; // Suppress this specific error
-  }
-  // Call the original console.error for other messages
-  originalConsoleError(...args);
-};
+// // Override console.error
+// console.error = (...args) => {
+//   // Suppress specific error messages
+//   if (/defaultProps/.test(args[0])) {
+//     return; // Suppress this specific error
+//   }
+//   // Call the original console.error for other messages
+//   originalConsoleError(...args);
+// };
